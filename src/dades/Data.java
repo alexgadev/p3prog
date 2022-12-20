1a4b5aa7d549fc8b3b476538f75522fe802f2de2
Sergi Llobet <sergi.llobet@urv.cat>
2022-12-20 20:22:36 +0100

    Sergi: prova1

---
+++ src/dades/Data.java
@@ -0,0 +1,22 @@
+package dades;
+
+public class Data {
+    private int dia, mes, any;
+
+    public Data(int dia, int mes, int any){
+        this.dia = dia;
+        this.mes = mes;
+        this.any = any;
+    }
+
+    public Data(){
+        this.dia = 1;
+        this.mes = 1;
+        this.any = 2023;
+    }
+
+    public String toString() {
+        return("DATA => dia "+dia+" mes "+mes+" any "+any);
+    }
+    
+    
