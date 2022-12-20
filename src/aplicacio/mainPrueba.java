b50f7a3769fe45ebab4fc235a98c935c9eac23f9
Jaume Tello Viñas <jaume.tello@estudiants.urv.com>
2022-12-20 20:12:47 +0100

    Proba

--- src/aplicacio/mainPrueba.java
+++ src/aplicacio/mainPrueba.java
@@ -13,7 +13,7 @@ public class mainPrueba {
         Usuari u2 = new Usuari("tomasTurbao", "foimbap@gmai.com", "34562");
         Usuari u3 = new Usuari("CryptoBro", "cryptobro@penemail.com", "35372");
 
-        Producte p1 = new Producte("taula", "hola", "be", 13/12/22);
+        //Producte p1 = new Producte("taula", "hola", "be", 13/12/22);
 
         llistaUsu.afegeixUsuari(u1);
         llistaUsu.afegeixUsuari(u2);
