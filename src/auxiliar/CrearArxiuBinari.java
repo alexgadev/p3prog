e93e30ea65665e55d280e1a85f92d2921998489e
Ivan Arenal <ivan.arenal@estudiants.urv.cat>
2022-12-22 16:13:19 +0100

    Implementació de la classe per crear un fitxer binari

--- src/auxiliar/CrearArxiuBinari.java
+++ src/auxiliar/CrearArxiuBinari.java
@@ -5,9 +5,18 @@ import java.util.Scanner;
 public class CrearArxiuBinari {
     
     public static void main(String[] args) throws IOException {
-        Scanner fitxerBinari = new Scanner(new File("Usuaris.txt"));
-        ObjectOutputStream fitxerSortida = new ObjectOutputStream(new FileOutputStream("Usuaris.ser"));
-        
-        
+        Scanner fit = new Scanner(new File("Usuaris.txt"));
+        ObjectOutputStream fitxerBinari = new ObjectOutputStream(new FileOutputStream("Usuaris.bin"));
+        String frase;
+
+        while (fit.hasNextLine()) {
+            frase = fit.nextLine();
+            System.out.println("Linia llegida! "+frase);
+
+            fitxerBinari.writeObject(frase); //Escrivim el contingut del fitxer mentre no sigui final
+        }
+        System.out.println("Fitxer generat!");
+        fit.close();
+        fitxerBinari.close();
     }
 }
