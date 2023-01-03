91ae87d9ef48461d770c26e786bd315ccc2f3b94
Ivan Arenal <ivan.arenal@estudiants.urv.cat>
2023-01-04 00:01:49 +0100

    Ivan: creació de la classe carregarUsuaris, la qual llegeix de la llista d'Usuaris tot el contingut i ho carrega en un fitxer binari

---
+++ src/auxiliar/carregarUsuaris.java
@@ -0,0 +1,43 @@
+package auxiliar;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import dades.Usuari;
+import dades.llistaUsuaris;
+
+public class carregarUsuaris {
+    public llistaUsuaris carregarUsu() throws IOException {
+        llistaUsuaris llistaAux = new llistaUsuaris(100);
+        boolean finalFitxer = false;
+        ObjectInputStream fitxerBinari;
+        Usuari usu;
+        
+        try{
+            fitxerBinari = new ObjectInputStream(new FileInputStream("Usuaris.bin"));
+            try{
+
+                while(!finalFitxer){
+                    usu = (Usuari)fitxerBinari.readObject();
+                    llistaAux.afegirUsuari(usu);
+                }
+
+            } catch(EOFException e){
+                finalFitxer = true;
+            } catch (ClassNotFoundException e){
+                System.err.println(e);
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("Fitxer no trobat!");
+        }
+        
+
+        
+        
+        
+        return llistaAux;
+    }
+}
